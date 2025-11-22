80ebceab074be673e5e2656d1f785ce4b7e95af7
MikajyRz <razafymikajy@gmail.com>
2025-11-22 09:43:18 +0300

    Sprint5

--- src/com/utils/MappingHandler.java
+++ src/com/utils/MappingHandler.java
@@ -6,26 +6,32 @@ public class MappingHandler {
     private Class<?> classe;
     private Method methode;
 
+    // Constructeur par défaut sans initialisation
     public MappingHandler() {
     }
 
+    // Constructeur qui enregistre la classe contrôleur et la méthode à appeler
     public MappingHandler(Class<?> classe, Method methode) {
         this.classe = classe;
         this.methode = methode;
     }
 
+    // Retourne la classe contrôleur associée à l'URL
     public Class<?> getClasse() {
         return classe;
     }
 
+    // Définit la classe contrôleur associée à l'URL
     public void setClasse(Class<?> classe) {
         this.classe = classe;
     }
 
+    // Retourne la méthode du contrôleur associée à l'URL
     public Method getMethode() {
         return methode;
     }
 
+    // Définit la méthode du contrôleur associée à l'URL
     public void setMethode(Method methode) {
         this.methode = methode;
     }
